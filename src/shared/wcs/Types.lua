39cca9488b5f62d053932bb96d0da5333bbf1610
Thomas Schollenberger <tom@schollenbergers.com>
2022-03-25 17:55:02 -0400

    fix typing issues, add asserts to each public facing function

--- src/shared/wcs/Types.lua
+++ src/shared/wcs/Types.lua
@@ -1,28 +1,30 @@
 local Signal = require(script.Parent.Signal)
+local Trove = require(script.Parent.Trove)
 
 export type ComponentInstance = {
-    __Initialized: boolean,
-    __InitializedSignal: typeof(Signal),
+	__Initialized: boolean,
+	__InitializedSignal: typeof(Signal),
 
-    Name: string,
-    Tag: string,
-    Needs: {string}, -- The required needs of the component
+	Name: string,
+	Tag: string,
+	Needs: {string}, -- The required needs of the component
+	Cleaner: typeof(Trove)?,
 
-    CreateDependencies: () -> {[string]: Instance},
-    Start: () -> (),
-    Destroy: () -> (),
+	CreateDependencies: () -> {[string]: Instance},
+	Start: () -> (),
+	Destroy: () -> (),
 }
 
 export type ComponentClass = {
-    Name: string, -- The name of the component
-    Tag: string, -- The tag that collection service should bind to
-    Ancestor: Instance?,
-    Needs: {string}, -- The required needs of the component
-    
-    new: (Instance) -> ComponentInstance, -- constructor
-    CreateDependencies: () -> {[string]: Instance}, -- Returns a dictionary where the keys are component names and the values are component roots
-    Start: () -> (), -- ran after .new and :CreateDependencies
-    Destroy: () -> (), -- ran when the entity loses the tag or is destroyed
+	Name: string, -- The name of the component
+	Tag: string, -- The tag that collection service should bind to
+	Ancestor: Instance?,
+	Needs: {string}, -- The required needs of the component
+
+	new: (Instance) -> ComponentInstance, -- constructor
+	CreateDependencies: (ComponentClass) -> {[string]: Instance}, -- Returns a dictionary where the keys are component names and the values are component roots
+	Start: (ComponentClass) -> (), -- ran after .new and :CreateDependencies
+	Destroy: (ComponentClass) -> (), -- ran when the entity loses the tag or is destroyed
 }
 
 return {}
--- src/shared/wcs/init.lua
+++ src/shared/wcs/init.lua
@@ -1,4 +1,4 @@
---!strict
+--!nonstrict
 
 local CollectionService = game:GetService("CollectionService")
 
@@ -13,120 +13,124 @@ type ComponentInstance = Types.ComponentInstance
 type ComponentClass = Types.ComponentClass
 
 local instances_on_components: {[Instance]: {[ComponentClass]: ComponentInstance}} = {}
-local component_names_on_components: {[string]: ComponentClass}
+local component_names_on_components: {[string]: ComponentClass} = {}
 
-local function _get_instance_component(instance: Instance, component: ComponentClass)
-    return (instances_on_components[instance] ~= nil and instances_on_components[instance][component]) or nil
+local function _get_instance_component(instance: Instance, component: ComponentClass): ComponentInstance | nil
+	if instances_on_components[instance] ~= nil then
+		return instances_on_components[instance][component]
+	end
+	return nil
 end
 
-local function get_component(instance: Instance, component_name: string): ComponentInstance
-    local component_class = component_names_on_components[component_name]
+local function get_component(instance: Instance, component_name: string): ComponentInstance | nil
+    assert(instance, "No instance provided")
+    assert(component_name, "No component name provided")
+    assert(typeof(instance) == "Instance", "Instance provided is not an instance")
+    assert(typeof(component_name) == "string", "Component name provided is not a string")
+    
+	local component_class = component_names_on_components[component_name]
 
-    local component_instance = _get_instance_component(instance, component_class)
-    if component_instance ~= nil then
-        if component_instance.__Initialized then
-            return component_instance
-        else
-            local proxy = Signal.new()
-            local initialized = false
-            local initialized_connection = component_instance.__InitializedSignal:Connect(function()
-                initialized = true
-                proxy:Fire(0)
-            end)
+	local component_instance = _get_instance_component(instance, component_class)
+	if component_instance ~= nil then
+		if component_instance.__Initialized == true then
+			return component_instance
+		else
+			local proxy = Signal.new()
+			local initialized = false
+			local initialized_connection = component_instance.__InitializedSignal:Connect(function()
+				initialized = true
+				proxy:Fire(0)
+			end)
 
-            task.delay(COMPONENT_START_TIMEOUT, function()
-                if initialized == false then
-                    proxy:Fire(1)
-                end
-            end)
+			task.delay(COMPONENT_START_TIMEOUT, function()
+				if initialized == false then
+					proxy:Fire(1)
+				end
+			end)
 
-            local result = proxy:Wait()
-            initialized_connection:Destroy()
+			local result = proxy:Wait()
+			initialized_connection:Destroy()
 
-            if result == 0 then
-                return component_instance
-            elseif result == 1 then
-                error("Initialization failed on " .. component_instance.Name)
-            end
-        end
-    end
+			if result == 0 then
+				return component_instance
+			elseif result == 1 then
+				error("Initialization failed on " .. component_instance.Name)
+			end
+		end
+	end
 
-    return nil
+	return nil
 end
 
 local function _create(instance: Instance, component: ComponentClass)
-    if instances_on_components[instance] == nil then
-        instances_on_components[instance] = {}
-    end
+	if instances_on_components[instance] == nil then
+		instances_on_components[instance] = {}
+	end
 
-    if instances_on_components[instance][component] == nil then
-        instances_on_components[instance][component] = {}
-    end
+	local component_instance = component.new(instance) :: ComponentInstance -- .new is ran syncronously
+	instances_on_components[instance][component] = component_instance
 
-    local component_instance = component.new(instance) :: ComponentInstance -- .new is ran syncronously
-    instances_on_components[instance][component] = component_instance
+	task.spawn(function() -- spawn a new thread to handle 
+		component_instance.__Initialized = false
+		local initialized_signal = Signal.new()
+		component_instance.__InitializedSignal = initialized_signal
 
-    task.spawn(function() -- spawn a new thread to handle 
-        component_instance.__Initialized = false
-        local initialized_signal = Signal.new()
-        component_instance.__InitializedSignal = initialized_signal
+		for componentName, componentRoot in pairs(component_instance:CreateDependencies()) do -- loop through the dependencies table
+			component_instance[componentName] = get_component(componentRoot, componentName) -- add each dependency into the component_instance
+		end
 
-        for componentName, componentRoot in pairs(component_instance:CreateDependencies()) do -- loop through the dependencies table
-            component_instance[componentName] = get_component(componentRoot, componentName) -- add each dependency into the component_instance
-        end
+		for _, need in pairs(component_instance.Needs) do -- loop through the needs
+			if need == "Cleaner" then
+				component_instance.Cleaner = Trove.new() -- create a cleaner and throw it into the component_instance
+			end
+		end
 
-        for _, need in pairs(component_instance.Needs) do -- loop through the needs
-            if need == "Cleaner" then
-                component_instance.Cleaner = Trove.new() -- create a cleaner and throw it into the component_instance
-            end
-        end
+		component_instance:Start() -- start the component sync'd in the thread
+		component_instance.__Initialized = true -- set the initialized variable to true and fire the event
+		initialized_signal:Fire()
+	end)
 
-        component_instance:Start() -- start the component sync'd in the thread
-        component_instance.__Initialized = true -- set the initialized variable to true and fire the event
-        initialized_signal:Fire()
-    end)
-
-    return component_instance
+	return component_instance
 end
 
 local function _destroy(component: ComponentInstance) -- destruction method wrapper
-    component:Destroy()
+	component:Destroy()
 end
 
 local function create_component(component: ComponentClass)
-    assert(component.Tag ~= nil, "Missing Tag property on " .. component)
-    assert(component.Name ~= nil, "Missing Name property on " .. component .. " with tag " .. component.Tag)
-    assert(component.new ~= nil, "Missing constructor on " .. component.Name)
-    assert(component.Initial ~= nil, "Missing initial function on " .. component.Name)
-    assert(component.Destroy ~= nil, "Missing destructor function on " .. component.Name)
+	assert(component.Tag ~= nil, "Missing Tag property")
+	assert(component.Name ~= nil, "Missing Name property on component with tag " .. component.Tag)
+	assert(component.new ~= nil, "Missing constructor on " .. component.Name)
+	assert(component.Start ~= nil, "Missing initial function on " .. component.Name)
+	assert(component.Destroy ~= nil, "Missing destructor function on " .. component.Name)
 
-    local ancestor = component.Ancestor
-    if ancestor == nil then
-        ancestor = game
-    end
+	local ancestor = component.Ancestor
+	if ancestor == nil then
+		ancestor = game
+	end
 
-    component_names_on_components[component.Name] = component
+	component_names_on_components[component.Name] = component
 
-    for _, thing in ipairs(CollectionService:GetTagged(component.Tag)) do
-        _create(thing, component)
-    end
+	for _, thing in ipairs(CollectionService:GetTagged(component.Tag)) do
+		_create(thing, component)
+	end
 
-    -- wait a frame to avoid double firing
-    task.wait()
+	-- wait a frame to avoid double firing
+	task.wait()
 
-    CollectionService:GetInstanceAddedSignal(component.Tag):Connect(function(instance)
-        _create(instance, component)
-    end)
+	CollectionService:GetInstanceAddedSignal(component.Tag):Connect(function(instance)
+		_create(instance, component)
+	end)
 
-    CollectionService:GetInstanceRemovedSignal(component.Tag):Connect(function(instance)
-        local component_instance = _get_instance_component(instance, component)
-        if component_instance ~= nil then
-            _destroy(component_instance)
-        end
-    end)
+	CollectionService:GetInstanceRemovedSignal(component.Tag):Connect(function(instance)
+		local component_instance = _get_instance_component(instance, component)
+		if component_instance ~= nil then
+			_destroy(component_instance)
+		end
+	end)
 end
 
 return {
-    create_component,
-    get_component
+	create_component,
+	get_component
 }
