45d262d89c7c758bbc5dc02691f8380947ea1bf6
= <=>
2022-06-27 11:30:39 -0400

    work changes

--- core/server/Components/Player/SkillHandler.lua
+++ core/server/Components/Player/SkillHandler.lua
@@ -2,10 +2,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local tcs = require(ReplicatedStorage:WaitForChild("Shared"):WaitForChild("tcs"))
 
-local RemoteFolder = Instance.new("Folder")
-RemoteFolder.Name = "SkillRemoteFolder"
-RemoteFolder.Parent = ReplicatedStorage
-
 local function setTableTransparency(player, objTable, transparency)
 	if transparency then
 		for i,v in pairs(objTable) do
@@ -59,19 +55,48 @@ SkillHandler.Ancestor = game
 function SkillHandler.new(root: any)
     return setmetatable({
         Root = root,
+	    CurrentSkill = "",
     }, SkillHandler)
 end
 
-function SkillHandler:Start()
-    local InvisRemote = Instance.new("RemoteEvent")
-    InvisRemote.Name = self.Root.Name.."_InvisRemote"
-    InvisRemote.Parent = RemoteFolder
+function SkillHandler:Start()         
+    local PlayerFolder = ReplicatedStorage:WaitForChild("PlayerFolders"):WaitForChild(self.Root.Name)    
 
+    local RemoteFolder = Instance.new("Folder")
+    RemoteFolder.Name = "SkillRemoteFolder"
+    RemoteFolder.Parent = PlayerFolder
+
+    local function createRemote(name: string, playerFolder: Folder)
+        local remote = Instance.new("RemoteEvent")
+        remote.Name = name
+        remote.Parent = RemoteFolder
+
+        return remote
+    end
+
+    local SkillEquippedRemote = createRemote(self.Root.Name.."_SkillEquippedRemote")
+    self.Cleaner:Add(SkillEquippedRemote.OnServerEvent:Connect(function(player, skillName: string)
+        if self.CurrentSkill == "APS" then
+            deleteSkillModel(player)
+        end
+
+        self.CurrentSkill = skillName
+    end))
+
+    local InvisRemote = createRemote(self.Root.Name.."_InvisRemote")
     self.Cleaner:Add(InvisRemote.OnServerEvent:Connect(function(player, objTable, transparency)
         if player ~= self.Root then player:Kick() end
         
         if player:GetAttribute("EquippedSkill") == "INVI-C" then
             setTableTransparency(player, objTable, transparency)
+        end
+    end))
+
+    local ShieldRemote = createRemote(self.Root.Name.."_ShieldRemote")
+    self.Cleaner:Add(ShieldRemote.OnServerEvent:Connect(function(player, request: string)
+        request = string.lower(request)
+     	if request == "create" then
+     	    createShieldModel(player)       
         end
     end))
 end
--- core/server/Components/Player/init.lua
+++ core/server/Components/Player/init.lua
@@ -4,6 +4,10 @@ local Shared = ReplicatedStorage:WaitForChild("Shared", 5)
 
 local tcs = require(Shared:WaitForChild("tcs", 5))
 
+local PlayerFolders = Instance.new("Folder")
+PlayerFolders.Name = "PlayerFolders"
+PlayerFolders.Parent = ReplicatedStorage
+
 local playerLoadedSignal = Instance.new("RemoteEvent")
 playerLoadedSignal.Name = "PlayerLoaded"
 playerLoadedSignal.Parent = ReplicatedStorage.Shared
@@ -24,11 +28,17 @@ end
 function Player:Start()
     self.Player:SetAttribute("Loaded", false)
 
+    local playerFolder = Instance.new("Folder")
+    playerFolder.Name = self.Root.Name
+    playerFolder.Parent = PlayerFolders
+
     self.Cleaner:Add(playerLoadedSignal.OnServerEvent:Connect(function(player: Player)
         if self.Player == player then
             self.Player:SetAttribute("Loaded", true)
         end
     end))
+
+    self.Cleaner:Add(playerFolder)
 end
 
 function Player:Destroy()
