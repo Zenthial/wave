056936a8d1acdba5773c5d2de107777b37c997fe
Thomas Schollenberger <tom@schollenbergers.com>
2022-06-30 23:40:16 -0400

    tcl

---
+++ core/shared/tcl/client.lua
@@ -0,0 +1,28 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Signal = require(ReplicatedStorage:WaitForChild("Shared"):WaitForChild("util"):WaitForChild("Signal"))
+local Trove = require(ReplicatedStorage:WaitForChild("Shared"):WaitForChild("util"):WaitForChild("Trove"))
+
+local PortRemote = ReplicatedStorage:WaitForChild("Shared"):WaitForChild("PortsFolder"):WaitForChild("PortRemote") :: RemoteFunction
+
+local TCLClient = {}
+
+function TCLClient:Send(portString: string, ...)
+    local portRemote = PortRemote:InvokeServer(portString) :: RemoteEvent | nil
+
+    if portRemote ~= nil then
+        portRemote:FireServer(...)
+    end
+end
+
+function TCLClient:Listen(portString: string)
+    local portRemote = PortRemote:InvokeServer(portString) :: RemoteEvent | nil
+
+    if portRemote ~= nil then
+        portRemote:FireServer(...)
+    else
+        return nil
+    end
+end
+
+return TCLClient
