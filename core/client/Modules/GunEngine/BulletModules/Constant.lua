7875f03921b61882d63dd70458560c4ae1c0be8c
= <=>
2022-07-07 19:13:47 -0400

    commit .swp

--- core/client/Modules/GunEngine/BulletModules/Constant.lua
+++ core/client/Modules/GunEngine/BulletModules/Constant.lua
@@ -20,12 +20,23 @@ Constant.__Tag = "Constant"
 
 function Constant.new(gunModel: GunModel, weaponStats)
     local storedBullet = weaponStats.BulletCache:GetPart()
-    storedBullet.CFrame = CF_REALLY_FAR_AWAY
+    local storedBullet2 = weaponStats.BulletCache:GetPart()
+    CollectionService:AddTag(storedBullet, "Ignore")
+    CollectionService:AddTag(storedBullet2, "Ignore")
+
+    local oldBulletScale = bullet.Mesh.Scale
+    local oldBulletOffset = bullet.Mesh.Offset
+    local oldBulletCFrame = bullet.CFrame
     
     local self = setmetatable({
         GunModel = gunModel,
         WeaponStats = weaponStats,
-        StoredBullet = storedBullet,        
+        StoredBullet = storedBullet,
+        StoredBullet2 = storedBullet2,
+
+        BulletScale = oldBulletScale,
+        BulletCFrame = CF_REALLY_FAR_AWAY,
+        BulletOffset = oldBulletOffset,
         
         Cleaner = Trove.new()
     }, Constant)
@@ -48,8 +59,45 @@ end
 
 function Constant.StaticDraw(startPosition: Vector3, endPosition: Vector3, bulletCache: PartCache.PartCache)
     math.randomseed(tick())
+ 
 
+
+    local bullet = self.StoredBullet :: BasePart 
+    local iDist = (endPosition - startPosition).Magnitude
     
+    local oldBulletScale = self.BulletScale
+    bullet.Mesh.Scale = Vector3.new(bullet.Mesh.Scale.X, bullet.Mesh.Scale.Y, iDist)
+    local oldBulletCFrame = self.BulletCFrame
+    bullet.CFrame = CFrame.new(startPosition, endPosition) * CFrame.new(0, 0, -iDist / 2)
+    
+    local x = bullet.Mesh.Scale.X
+    local y = bullet.Mesh.Scale.Y
+    local z = bullet.Mesh.Scale.Z
+    local bullet2 = self.StoredBullet2
+    local oldBullet2Offset = self.BulletOffset
+    bullet2.Mesh.Scale = Vector3.new(bullet.Mesh.Scale.X, bullet.Mesh.Scale.Y, iDist)
+    bullet2.CFrame = CFrame.new(startPosition, endPosition) * CFrame.new(0, 0, -iDist / 2)
+    local scale = z * .5
+    local offset = -z * .25
+    if z > 100 then
+        scale = z - 50
+        offset = -50
+    end
+    bullet2.Mesh.Scale = Vector3.new(x, y, scale)
+    bullet2.Mesh.Offset = Vector3.new(0, 0, offset)
+    
+    task.delay(.05, function()
+        bullet.Mesh.Scale = oldBulletScale
+        bullet.CFrame = oldBulletCFrame
+        bulletCache:ReturnPart(bullet)
+    end)
+
+    task.delay(.1, function()
+        bullet2.Mesh.Scale = oldBulletScale
+        bullet2.Mesh.Offset = oldBullet2Offset
+        bullet2.CFrame = oldBulletCFrame
+        bulletCache:ReturnPart(bullet2)
+    end)
 end
 
 function Constant:Destroy()
