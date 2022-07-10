4bf88b68493be137f292926e4bef7507ecd88224
colorfulparadox <71807049+mrtreefroggo@users.noreply.github.com>
2022-07-10 10:17:36 -0700

    First Nametag Push
    First
    
    I am going to break the name tag up further into more components to make it easier to create pings/waypoints

--- core/client/Components/UI/Nametag.lua
+++ core/client/Components/UI/Nametag.lua
@@ -4,86 +4,75 @@ local uiAssets = ReplicatedStorage:WaitForChild("Assets", 5).UI
 local nameTag = uiAssets:WaitForChild("NameTag", 5)
 
 local tcs = require(ReplicatedStorage:WaitForChild("Shared"):WaitForChild("tcs"))
-
 type Cleaner_T = {
-	Add: (Cleaner_T, any) -> (),
-	Clean: (Cleaner_T) -> ()
+    Add: (Cleaner_T, any) -> (),
+    Clean: (Cleaner_T) -> ()
 }
 
 type Courier_T = {
-	Listen: (Courier_T, Port: string) -> {Connect: RBXScriptSignal},
-	Send: (Courier_T, Port: string, ...any) -> ()
+    Listen: (Courier_T, Port: string) -> {Connect: RBXScriptSignal},
+    Send: (Courier_T, Port: string, ...any) -> ()
 }
 
-type Nametag_T = {
-	__index: Nametag_T,
-	Name: string,
-	Tag: string,
+type NameTag_T = {
+    __index: NameTag_T,
+    Name: string,
+    Tag: string,
 
-	Cleaner: Cleaner_T,
-	Courier: Courier_T
+    Cleaner: Cleaner_T,
+    Courier: Courier_T
 }
 
-local Nametag: Nametag_T = {}
-Nametag.__index = Nametag
-Nametag.Name = "Nametag"
-Nametag.Tag = "Nametag"
-Nametag.Ancestor = game
+local NameTag: NameTag_T = {}
+NameTag.__index = NameTag
+NameTag.Name = "NameTag"
+NameTag.Tag = "NameTag"
+NameTag.Ancestor = game
 
-function Nametag.new(root: any)
-	local folder = nameTag:Clone()
-
-	return setmetatable({
-		Root = root,
-		NameTag = folder,
-		HealthUI = folder.HealthUI,
-		Highlight = folder.Highlight
-	}, Nametag)
+function NameTag.new(root: any)
+    return setmetatable({
+        Root = root,
+        Tag = nameTag:Clone()
+    }, NameTag)
 end
 
-function Nametag:Start()
-	self.NameTag.Parent = self.Root
-	self.NameTag:SetAttribute("Enabled", false)
-	self.NameTag:SetAttribute("LastTick", 0)
-
-	self:SetAdornee(self.Root)
-	self:Enable()
+function NameTag:Start()
+	self.Tag.Parent = self.Root
+	self.Tag:SetAttribute("Enabled", false)
+	self.Tag:SetAttribute("LastTick", 0)
 
 	--blah blah blah this should be inside of destroy but who cares
 	--this looks really nice though
 	self.Cleaner:Add(function() 
-		self.NameTag:Destroy()
+		self.Tag:Destroy()
 	end)
 end
 
-function Nametag:SetAdornee(int: any)
-	self.HealthUI.Adornee = int
-	self.Highlight.Adornee = int
+function NameTag:SetAdornee(int: any)
+	self.Tag.Adornee = int
 end
 
-function Nametag:SetColor(healthColor: Color3, nameTagColor: Color3)
-	print("color change")
-
+function NameTag:SetName(str: string)
+	self.Tag.TagText.Text = str
 end
 
-function Nametag:Enable()
-	print("nametag enabled")
-
-	self.HealthUI.Enabled = true
-	self.Highlight.Enabled = true
+function NameTag:SetColor(healthColor: Color3, nameTagColor: Color3)
+	self.Tag.Bar.Frame.BackgroundColor3 = healthColor
+	self.Tag.TagText.TextColor3 = nameTagColor
 end
 
-function Nametag:Disable()
-	print("nametag disabled")
-
-	self.HealthUI.Enabled = false
-	self.Highlight.Enabled = false
+function NameTag:Enable()
+	self.Tag.Enabled = true
 end
 
-function Nametag:Destroy()
-	self.Cleaner:Clean()
+function NameTag:Disable()
+	self.Tag.Enabled = false
 end
 
-tcs.create_component(Nametag)
+function NameTag:Destroy()
+    self.Cleaner:Clean()
+end
 
-return Nametag
+tcs.create_component(NameTag)
+
+return NameTag
---
+++ core/client/Modules/NametagController.lua
@@ -0,0 +1,103 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+local Players = game:GetService("Players")
+local Teams = game:GetService("Teams")
+
+local util = ReplicatedStorage:WaitForChild("Shared").util
+local promise = require(util.Promise)
+
+local tcs = require(ReplicatedStorage:WaitForChild("Shared"):WaitForChild("tcs"))
+
+local client = Players.LocalPlayer
+
+local NametagController = {}
+NametagController.__index = NametagController
+
+local function setTagToEnemy(nameTag)
+    nameTag:SetColor(Color3.fromRGB(254, 85, 85), Color3.fromRGB(254, 85, 85))
+end
+
+local function setTagToAlly(nameTag)
+    nameTag:SetColor(Color3.fromRGB(96, 255, 122), Color3.fromRGB(255,255,255))
+end
+
+local function playerAdded(player: Player)
+    local function changeTeam(prop: string)    
+        if  player.Character == nil then return end 
+        if prop ~= "TeamColor" then return end
+        
+        local nameTag = tcs.get_component(player.Character, "NameTag")
+
+        if nameTag == nil then return end
+
+        if player.TeamColor ~= client.TeamColor then
+            nameTag:Disable()
+            setTagToEnemy(nameTag)
+            return
+        end
+
+        nameTag:Enable()
+        setTagToAlly(nameTag)
+    end 
+
+    local function characterAdded(character)
+        CollectionService:AddTag(character, "NameTag")
+    
+        local nameTag = tcs.get_component(character, "NameTag")
+    
+        nameTag:SetName(player.DisplayName)
+        nameTag:SetAdornee(character.Head)
+
+        changeTeam("TeamColor")
+    end
+
+    if player.Character then
+        characterAdded(player.Character)
+    end
+    player.CharacterAdded:Connect(characterAdded)
+    player.Changed:Connect(changeTeam)
+end
+
+local function clientTeamChange(prop: string)
+    if prop ~= "TeamColor" then return end
+
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player == client then continue end
+
+        local nameTag = tcs.get_component(player.Character, "NameTag")
+
+        if nameTag == nil then return end
+
+        if player.TeamColor ~= client.TeamColor then
+            nameTag:Disable()
+            setTagToEnemy(nameTag)
+            continue
+        end
+
+        nameTag:Enable()
+        setTagToAlly(nameTag)
+    end
+end
+
+function NametagController.new()
+    local self = setmetatable({}, NametagController)
+    return self
+end
+
+function NametagController:Start()
+    
+    for _, player in ipairs(Players:GetPlayers()) do
+        if player == client then continue end
+        playerAdded(player)
+    end
+    Players.PlayerAdded:Connect(playerAdded)
+
+    clientTeamChange("TeamColor")
+    client.Changed:Connect(clientTeamChange)
+end
+
+function NametagController:Destroy()
+    
+end
+
+return NametagController
